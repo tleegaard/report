29dc46698df6dd5d984b838d604fad0f7d89f457
tleegaard <tleegaard@gmail.com>
2013-09-25 22:32:41 +0200

    Minor error

--- report_menu.php
+++ report_menu.php
@@ -1,5 +1,5 @@
 <?php
 
-  $menu_dropdown[] = array('name'=>"<?php echo _('Report'); ?>", 'path'=>"report/electric" , 'session'=>"write", 'order' => 7 );
+  $menu_dropdown[] = array('name'=> _("Report"), 'path'=>"report/electric" , 'session'=>"write", 'order' => 7 );
   //$menu_dropdown[] = array('name'=>"Appliance List", 'path'=>"report/applist" , 'session'=>"write", 'order' => 8 );
 ?>
